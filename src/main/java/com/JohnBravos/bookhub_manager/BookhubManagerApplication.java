e2c92b771d885e80163cbb4a1290ef520d6b0667
bravosgiannis <bravosgiannis@gmail.com>
2025-10-22 19:58:41 +0300

    Initial Spring Boot project setup

---
+++ src/main/java/com/JohnBravos/bookhub_manager/BookhubManagerApplication.java
@@ -0,0 +1,13 @@
+package com.JohnBravos.bookhub_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookhubManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookhubManagerApplication.class, args);
+	}
+
+}
