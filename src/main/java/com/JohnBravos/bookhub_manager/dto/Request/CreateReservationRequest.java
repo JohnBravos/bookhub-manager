4aada2829d2efcc1f57eea201849689bb7981653
bravosgiannis <bravosgiannis@gmail.com>
2025-10-30 00:02:17 +0200

    chore: changed name to "CreateReservationRequest" from "CreateReservationResponse"

--- src/main/java/com/JohnBravos/bookhub_manager/dto/Request/CreateReservationRequest.java
+++ src/main/java/com/JohnBravos/bookhub_manager/dto/Request/CreateReservationRequest.java
@@ -2,7 +2,7 @@ package com.JohnBravos.bookhub_manager.dto.Request;
 
 import jakarta.validation.constraints.NotNull;
 
-public record CreateReservationResponse(
+public record CreateReservationRequest(
         @NotNull(message = "User ID is required")
         Long userId,
 
