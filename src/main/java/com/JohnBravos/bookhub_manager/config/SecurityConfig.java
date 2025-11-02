12e8ab49005d558588fe873eec83a5339a0ebd89
bravosgiannis <bravosgiannis@gmail.com>
2025-11-02 21:33:53 +0200

    feat: add SecurityConfig, PasswordEncoder removed SecurityFilterChain

--- src/main/java/com/JohnBravos/bookhub_manager/config/SecurityConfig.java
+++ src/main/java/com/JohnBravos/bookhub_manager/config/SecurityConfig.java
@@ -2,20 +2,15 @@ package com.JohnBravos.bookhub_manager.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class SecurityConfig {
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-                .authorizeHttpRequests(auth -> auth
-                .anyRequest().permitAll()
-        ).csrf(csrf -> csrf.disable());
-
-        return http.build();
+    public PasswordEncoder passwordEncoder()
+    {
+        return new BCryptPasswordEncoder();
     }
 }
