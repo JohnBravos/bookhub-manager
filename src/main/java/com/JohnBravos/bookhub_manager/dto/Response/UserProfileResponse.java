ae49e2852b54b47a116a26d4c0b25829e7724fda
bravosgiannis <bravosgiannis@gmail.com>
2025-11-02 21:29:42 +0200

    feat: add "int totalReservationCount" on UserProfileResponse

--- src/main/java/com/JohnBravos/bookhub_manager/dto/Response/UserProfileResponse.java
+++ src/main/java/com/JohnBravos/bookhub_manager/dto/Response/UserProfileResponse.java
@@ -1,14 +1,17 @@
 package com.JohnBravos.bookhub_manager.dto.Response;
 
 import com.JohnBravos.bookhub_manager.model.Reservation;
+import lombok.Builder;
 
 import java.util.List;
 
+@Builder
 public record UserProfileResponse(
 
         UserResponse user,
         int activeLoansCount,
         int totalLoansCount,
+        int totalReservationsCount,
         List<LoanResponse> currentLoans,
         List<ReservationResponse> currentReservations,
 
