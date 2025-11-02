0b8d212895b57e5478a4d03b5a4834ec0e3d30b5
bravosgiannis <bravosgiannis@gmail.com>
2025-11-02 21:23:16 +0200

    feat(request dto): add UpdateAuthorRequest

---
+++ src/main/java/com/JohnBravos/bookhub_manager/dto/Request/UpdateAuthorRequest.java
@@ -0,0 +1,23 @@
+package com.JohnBravos.bookhub_manager.dto.Request;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+import java.time.LocalDate;
+
+public record UpdateAuthorRequest(
+        @NotBlank(message = "First name is required")
+        @Size(min = 2, max = 50, message = "First name must be between 2 and 50 characters")
+        String firstName,
+
+        @NotBlank(message = "Last name is required")
+        @Size(min = 2, max = 50, message = "Last name must be between 2 and 50 characters")
+        String lastName,
+
+        String nationality,
+
+        LocalDate birthDate,
+
+        @Size(max = 2000, message = "Biography must not exceed 2000 characters")
+        String biography
+) {}
