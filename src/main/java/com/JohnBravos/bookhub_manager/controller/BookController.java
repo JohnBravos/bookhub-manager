cc80c72e5c81792fe17b5d250f17d6fdffd5d5be
bravosgiannis <bravosgiannis@gmail.com>
2025-11-14 20:04:57 +0200

    chore: replace getAllBooks with Page response entity

--- src/main/java/com/JohnBravos/bookhub_manager/controller/BookController.java
+++ src/main/java/com/JohnBravos/bookhub_manager/controller/BookController.java
@@ -11,6 +11,7 @@ import com.JohnBravos.bookhub_manager.service.IBookService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -29,11 +30,15 @@ public class BookController {
 
     // GET ALL BOOKS (Public)
     @GetMapping
-    public ResponseEntity<ApiResponse<List<BookResponse>>> getAllBooks() {
-        log.info("Fetching all books");
-        List<BookResponse> books = bookService.getAllBooks();
-        return ResponseEntity.ok(ApiResponse.success(books, "Books retrieved successfully"));
-    }
+    public ResponseEntity<ApiResponse<Page<BookResponse>>> getAllBooks(
+         @RequestParam(defaultValue = "0") int page,
+         @RequestParam(defaultValue = "10") int size,
+         @RequestParam(defaultValue = "id,asc") String sort
+) {
+        Page<BookResponse> booksPage = bookService.getAllBooks(page, size, sort);
+
+        return ResponseEntity.ok(ApiResponse.success(booksPage, "Books fetched successfully"));
+        }
 
     // GET BOOK BY ID (PUBLIC)
     @GetMapping("/{id}")
