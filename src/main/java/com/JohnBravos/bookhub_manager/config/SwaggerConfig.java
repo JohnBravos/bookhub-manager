8c836d5ca5f672900e9364a83e5c9129c1dd84c3
bravosgiannis <bravosgiannis@gmail.com>
2025-12-13 20:39:53 +0200

    Add security scheme, servers, and JWT authorization to SwaggerConfig

--- src/main/java/com/JohnBravos/bookhub_manager/config/SwaggerConfig.java
+++ src/main/java/com/JohnBravos/bookhub_manager/config/SwaggerConfig.java
@@ -1,8 +1,12 @@
 package com.JohnBravos.bookhub_manager.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.servers.Server;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -17,7 +21,21 @@ public class SwaggerConfig {
                         .description("Library Management System - REST API Documentation")
                         .version("1.0.0")
                         .contact(new Contact()
-                                .name("John Bravos")
-                                .email("john.bravos@example.com")));
+                                .name("IOANNIS BRAVOS")
+                                .email("bravosgiannis@gmail.com")))
+                .addServersItem(new Server()
+                        .url("http://localhost:8080/api")
+                        .description("Local Development Server"))
+                .addServersItem(new Server()
+                        .url("http://localhost:8080")
+                        .description("Local Server Root"))
+                .components(new Components()
+                        .addSecuritySchemes("bearer-jwt",
+                                new SecurityScheme()
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")
+                                        .description("JWT token authentication. Get token by login endpoint.")))
+                .addSecurityItem(new SecurityRequirement().addList("bearer-jwt"));
     }
 }
