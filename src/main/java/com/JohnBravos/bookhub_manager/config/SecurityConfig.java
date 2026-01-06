bd09cf6e09faa7849131ba4968206b16a906d97e
bravosgiannis <bravosgiannis@gmail.com>
2026-01-06 21:31:29 +0200

    feat: add cors for vercel

--- src/main/java/com/JohnBravos/bookhub_manager/config/SecurityConfig.java
+++ src/main/java/com/JohnBravos/bookhub_manager/config/SecurityConfig.java
@@ -103,7 +103,7 @@ public class SecurityConfig {
                 .csrf(csrf -> csrf.disable())
                 .cors(cors -> cors.configurationSource(request -> {
                     var config = new org.springframework.web.cors.CorsConfiguration();
-                    config.setAllowedOrigins(java.util.List.of("http://localhost:5174", "http://localhost:3000", "http://localhost:5173"));
+                    config.setAllowedOrigins(java.util.List.of("http://localhost:5174", "http://localhost:3000", "http://localhost:5173", "https://bookhub-manager.vercel.app"));
                     config.setAllowedMethods(java.util.List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
                     config.setAllowedHeaders(java.util.List.of("*"));
                     config.setAllowCredentials(true);
